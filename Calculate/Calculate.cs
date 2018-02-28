70be94a7f050219b84c7b340a82d1bdb887e9129
root <root@DESKTOP-VM58PUE.localdomain>
2018-02-28 11:49:09 +0700

    Add new project

---
+++ Calculate/Calculate.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+
+namespace Calculate
+{
+    class Calculate
+    {
+        double[] c;
+        int dem = 0;
+        String s = "";
+        Double Result = 0;
+        public Calculate(String st)
+        {
+            s = st;
+        }
+        private static bool IsEmpty(Stack c)
+        {
+            if (c.Count == 0)
+                return true;
+            else
+                return false;
+        }
+        private int prio(char a)
+        {
+            if (a == 'v' || a == '^')
+                return 4;
+            if (a == 'i' || a == 'c')
+                return 3;
+            if (a == '*' || a == '/')
+                return 2;
+            if (a == '+' || a == '-')
+                return 1;
+            return 0;
+        }
+        private void chuyenkyphap()
+        {
+            Stack a = new Stack();
+            c = new double[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= '0' && s[i] <= '9')
+                {
+                    double temp = 0;
+                    char ch = s[i];
+                    while (ch >= '0' && ch <= '9')
+                    {
+                        temp = temp * 10 + ch - 48;
+                        if (i >= s.Length - 1)
+                            break;
+                        else
+                        {
+                            i++;
+                            ch = s[i];
+                        }
+                    }
+                    if (s[i] == '.')
+                    {
+                        i++;
+                        ch = s[i];
+                        double r = 1;
+                        double t = 0;
+                        while (ch >= '0' && ch <= '9')
+                        {
+                            r = r / 10;
+                            t = t + ((s[i] - 48) * r);
+                            if (i >= s.Length - 1)
+                                break;
+                            else
+                            {
+                                i++;
+                                ch = s[i];
+                            }
+                        }
+                        temp = temp + t;
+                    }
+                    c[dem] = temp;
+                    dem++;
+                }
+                if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == 'v' || s[i] == 'i' || s[i] == 'c' || s[i] == '^')
+                {
+                    if (IsEmpty(a) == true)
+                        if (s[i] == 'r')
+                        {
+                            if (s[i + 2] == 's')
+                                a.Push('w');
+                            else
+                                a.Push('z');
+                            i = i + 4;
+                        }
+                        else
+                            a.Push((char)(s[i]));
+                    else
+                        if (prio(s[i]) <= prio((char)(a.Peek())))
+                    {
+                        c[dem] = (char)(a.Pop());
+                        dem++;
+                        if (s[i] == 'r')
+                        {
+                            if (s[i + 2] == 's')
+                                a.Push('w');
+                            else
+                                a.Push('z');
+                            i = i + 4;
+                        }
+                        else
+                            a.Push((char)(s[i]));
+                    }
+                    else
+                            if (s[i] == 'r')
+                    {
+                        if (s[i + 2] == 's')
+                            a.Push('w');
+                        else
+                            a.Push('z');
+                        i = i + 4;
+                    }
+                    else
+                        a.Push((char)(s[i]));
+                }
+                if (s[i] == '(')
+                    a.Push((char)(s[i]));
+                if (s[i] == ')')
+                {
+                    char ch;
+                    do
+                    {
+                        ch = Convert.ToChar(a.Pop());
+                        if (ch != '(')
+                        {
+                            c[dem] = ch;
+                            dem++;
+                        }
+                    }
+                    while (ch != '(');
+                }
+            }
+            while (IsEmpty(a) != true)
+            {
+                c[dem] = Convert.ToChar(a.Pop());
+                dem++;
+            }
+        }
+        private void ketqua()
+        {
+            Stack b = new Stack();
+            for (int i = 0; i < dem; i++)
+            {
+                switch ((int)c[i])
+                {
+                    //Sum operator
+                    case '+':
+                        {
+                            double a, k;
+                            a = (double)(b.Pop());
+                            k = (double)(b.Pop());
+                            b.Push(a + k);
+                            break;
+                        }
+                    //Subtract operator
+                    case '-':
+                        {
+                            double a, k;
+                            a = (double)(b.Pop());
+                            k = (double)(b.Pop());
+                            b.Push(k - a);
+                            break;
+                        }
+                    //Multiply operator
+                    case '*':
+                        {
+                            double a, k;
+                            a = (double)(b.Pop());
+                            k = (double)(b.Pop());
+                            b.Push(a * k);
+                            break;
+                        }
+                    //Division operator
+                    case '/':
+                        {
+                            double a, k;
+                            a = (double)(b.Pop());
+                            k = (double)(b.Pop());
+                            b.Push(k / a);
+                            break;
+                        }
+                    //Sin 
+                    case 'i':
+                        {
+                            double a;
+                            a = (double)(b.Pop());
+                            b.Push(Math.Sin((a / 180) * 3.14));
+                            break;
+                        }
+                    //Cos
+                    case 'c':
+                        {
+                            double a;
+                            a = (double)(b.Pop());
+                            b.Push(Math.Cos((a / 180) * 3.14));
+                            break;
+                        }
+                    //A Sin
+                    case 'w':
+                        {
+                            double a;
+                            a = (double)(b.Pop());
+                            b.Push((Math.Asin(a) / 3.14) * 180);
+                            break;
+                        }
+                    //A cos
+                    case 'z':
+                        {
+                            double a;
+                            a = (double)(b.Pop());
+                            b.Push((Math.Acos(a) / 3.14) * 180);
+                            break;
+                        }
+                    //Square root
+                    case 'v':
+                        {
+                            double a;
+                            a = (double)(b.Pop());
+                            b.Push(Math.Sqrt(a));
+                            break;
+                        }
+                    //Square
+                    case '^':
+                        {
+                            double a, k;
+                            a = (double)(b.Pop());
+                            k = (double)(b.Pop());
+                            b.Push(Math.Pow(k, a));
+                            break;
+                        }
+                    default:
+                        {
+                            b.Push((double)c[i]);
+                            break;
+                        }
+                }
+            }
+            Result = Convert.ToDouble(b.Pop());
+        }
+        public double returnKq()
+        {
+            chuyenkyphap();
+            ketqua();
+            return this.Result;
+        }
+    }
+}
---
+++ Calculate/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "+";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "-";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "*";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "/";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "^";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "v";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "sin";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "cos";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "Asin";
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += "Acos";
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            labelKq.Text = "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            String expr = txtInput.Text;
+            if (expr.Contains("sin"))
+                expr.Replace("sin", "i");
+            if (expr.Contains("cos"))
+                expr.Replace("cos", "c");
+            if (expr.Contains("Asin"))
+                expr.Replace("Asin", "w");
+            if (expr.Contains("Acos"))
+                expr.Replace("Acos", "z");
+
+            if (expr != "" && expr != null) {
+                Calculate cal = new Calculate(expr);
+                labelKq.Text = Convert.ToString(cal.returnKq());
+            }         
+        }
+    }
+}
